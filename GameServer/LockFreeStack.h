e71d9876d98a3af495d56bba0b25f94cd36deec3
ohyudan <ohyudan86@gmail.com>
2025-09-04 23:52:58 +0900

    fix: 1번 -> atomic 을 이용한 구현 가장 헷갈림

--- GameServer/LockFreeStack.h
+++ GameServer/LockFreeStack.h
@@ -4,7 +4,7 @@ template <typename T>
 class LockFreeStack
 {
 	struct Node {
-		Node(const T& value) : data(value) {
+		Node(const T& value) : data(value),next(nullptr) {
 
 		}
 		T data;
@@ -46,8 +46,8 @@ public:
 	//4) data를 반환한다.
 	//5) 노드를 삭제한다.
 	bool TryPop(T& value) {
+		++_popCount;
 		Node* oldHead = _head;
-
 
 		/*if (_head == oldHead) {
 			_head = oldHead->next;
@@ -59,7 +59,8 @@ public:
 		}*/
 		while (oldHead && _head.compare_exchange_weak(oldHead, oldHead->next) == false)
 		{
-
+			--_popCount;
+			return false;
 		};
 
 		if (oldHead == nullptr)
@@ -67,12 +68,69 @@ public:
 
 		value = oldHead->data;
 
-		//delete oldHead;
-		//GC 없음. 스마트포인터?...사용?
+
+		//delete oldHead;	
 		return true;
 	}
+	// 순서 보장 필요
+	// 1) 데이터분리
+	//2) Count 체크
+	//3) Count가 0이면 삭제
+	void TryDelete(Node* oldHead) {
+			//나 말고 있나
+		if (_popCount == 1) {
+						
+			Node* node = _pendingList.exchange(nullptr);
+			if (--_popCount == 0) {
+				//끼어든 애가 없음 -> 삭제
+				//이제 와서 끼어들어도 ,어쩌피 데이터는 분리해둔 상태
+				DeleteNodes(node);
+			}else if(node){
+				//재반환
+				ChainPendingNodeList(node);
+			}
 
+			delete oldHead;
+		}
+		else {
+					//누가 있다. 그럼 지금 삭제말고 나중에	
+			ChainPendingNode(oldHead);
+			--_popCount;
+		}
+	}
+
+	static void DeleteNodes(Node* node) {
+		
+while (node) {
+			Node* next = node->next;
+			delete node;
+			node = next;
+		}
+	}
+
+	void CainPendingNodeList(Node* first, Node* last) {
+				last->next = _pendingList;
+		while (_pendingList.compare_exchange_weak(last->next, first) == false)
+		{
+		};
+	}
+
+	void ChainPendingNodeList(Node* node) {
+		Node* last = node;
+		while(last->next) {
+			last = last->next;
+		}
+		ChainPendingNodeList(node, last);
+	}
+
+	void ChainPendingNode(Node* node) {
+		ChainPendingNodeList(node, node);
+	}
 private:
 	atomic<Node*> _head;
+
+	atomic<uint32> _popCount = 0; //Pop을 실행중인 쓰레드 개수
+
+	atomic<Node*> _pendingList; // 삭제되야될 노드들
 };
 
