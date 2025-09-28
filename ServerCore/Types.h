f77326e1a8485786d7f06606d2e96682d1b2dcda
ohyudan <ohyudan86@gmail.com>
2025-09-28 19:20:29 +0900

    fix: 멀티스레드 구조 개선 및 불필요한 코드 제거
    에서  관련 코드 제거 및 를 활용한 멀티스레드 기반 코드 추가. 를 사용한 동기화 처리 추가.
    
     및 에서  관련 파일 제거 및 새로운 필터(, ) 추가.

--- ServerCore/Types.h
+++ ServerCore/Types.h
@@ -1,4 +1,7 @@
 #pragma once
+
+#include <atomic>
+#include <mutex>
 //__언더바 작성하기 싫어서 이렇게 작성함
 using BYTE = unsigned char;
 using int8 = __int8;
@@ -10,3 +13,9 @@ using uint16 = unsigned __int16;
 using uint32 = unsigned __int32;
 using uint64 = unsigned __int64;
 
+template<typename T>
+using Atomic = std::atomic<T>;
+using Mutex = std::mutex;
+using CondVar = std::condition_variable;
+using UniqueLock = std::unique_lock<std::mutex>;
+using LockGuard = std::lock_guard<std::mutex>;
