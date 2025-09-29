5fc554463031f55f060df96d05fabe214b852375
ohyudan <ohyudan86@gmail.com>
2025-09-29 18:47:19 +0900

    fix: Macro 추가 / 필터 변경 / 헤더 windows Lock.h Macro 헤더 추가
    잠금 및 데드락 프로파일링 기능 추가
    
    `CoreMacro.h`에 잠금 관련 매크로(`USE_MANY_LOCKS`, `READ_LOCK`, `WRITE_LOCK` 등)와 충돌 처리용 `CRASH` 매크로가 추가되었습니다.
    `CorePch.h`에 `CoreMacro.h`, `windows.h`, `Lock.h` 헤더 파일이 포함되었으며, 잠금 및 데드락 프로파일링 관련 파일(`DeadLockProfiler.h`, `Lock.h`, `DeadLockProfiler.cpp`, `Lock.cpp`)이 `ServerCore.vcxproj`에 추가되었습니다.
    
    또한, `ServerCore.vcxproj.filters`에서 `ThreadManager` 필터가 `Thread` 필터로 대체되었으며, 관련 파일들이 해당 필터로 이동되었습니다.

--- ServerCore/CorePch.h
+++ ServerCore/CorePch.h
@@ -3,6 +3,7 @@
 #include "Types.h"
 #include "CoreGlobal.h"
 #include "CoreTLS.h"
+#include "CoreMacro.h"
 
 #include <vector>
 #include <list>
@@ -13,6 +14,8 @@
 #include <unordered_map>
 #include <unordered_set>
 
+#include <windows.h>
 #include <iostream>
 using namespace std;
 
+#include "Lock.h"
